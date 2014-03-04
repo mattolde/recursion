40858fcb2d24fb3230edf797b91ede44ecb92224
Matthew Olde <mattolde@gmail.com>
2014-03-04 15:25:26 +1000

    getElementByClassName completed.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,5 +5,36 @@
 
 // But in stead we're going to implement it from scratch:
 var getElementsByClassName = function (className) {
-  // your code here
+	// your code here
+
+	function walkDOM(node, callback) {
+
+		var children = node.childNodes;
+
+		for (var i = 0; i < children.length; i++) {
+			walkDOM(children[i], callback);
+		}
+
+		callback(node);
+	}
+
+	function isElement(node){
+		return (
+			typeof HTMLElement === "object" ? node instanceof HTMLElement : node && typeof node === "object" && node !== null && node.nodeType === 1 && typeof node.nodeName==="string"
+		);
+	}
+
+	var results = [];
+
+	walkDOM(document.body, function(node) {
+
+		if (isElement(node)){
+			if(node.classList.contains(className)) {
+				results.push(node);
+			}
+		}
+	});
+
+	return results;
+
 };
