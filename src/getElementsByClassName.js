c8b2f8b16a7bcefe482787538ea8a2f8e7b93228
Matthew Olde <mattolde@gmail.com>
2014-03-06 13:52:00 +1000

    Changed tabs to 2 instead of 4

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,36 +5,35 @@
 
 // But in stead we're going to implement it from scratch:
 var getElementsByClassName = function (className) {
-	// your code here
+  // your code here
 
-	function walkDOM(node, callback) {
+  function walkDOM(node, callback) {
 
-		var children = node.childNodes;
+    var children = node.childNodes;
 
-		for (var i = 0; i < children.length; i++) {
-			walkDOM(children[i], callback);
-		}
+    for (var i = 0; i < children.length; i++) {
+        walkDOM(children[i], callback);
+    }
 
-		callback(node);
-	}
+    callback(node);
+  }
 
-	function isElement(node){
-		return (
-			typeof HTMLElement === "object" ? node instanceof HTMLElement : node && typeof node === "object" && node !== null && node.nodeType === 1 && typeof node.nodeName==="string"
-		);
-	}
+  function isElement(node){
+    return (
+        typeof HTMLElement === "object" ? node instanceof HTMLElement : node && typeof node === "object" && node !== null && node.nodeType === 1 && typeof node.nodeName==="string"
+    );
+  }
 
-	var results = [];
+  var results = [];
 
-	walkDOM(document.body, function(node) {
+  walkDOM(document.body, function(node) {
+    if (isElement(node)){
+        if(node.classList.contains(className)) {
+            results.push(node);
+        }
+    }
+  });
 
-		if (isElement(node)){
-			if(node.classList.contains(className)) {
-				results.push(node);
-			}
-		}
-	});
-
-	return results;
+  return results;
 
 };
